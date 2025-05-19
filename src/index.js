fd6a8402402a2e1c4c60480e0bc506ca1e891208
LDunke <159555008+LDunke@users.noreply.github.com>
2025-05-19 19:35:54 +0000

    Fim da Vídeo Aula 3

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ const server = express();
 const PORT = process.env.PORT || 3000;
 
 server.get("/", (req, res)=>{
-    res.status(200).send("Primeira Rota do Backend - Alterado"); });
+    res.status(200).send("Primeira Rota do Backend");
+});
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
